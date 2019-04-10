b14bd9a104e3ed60a8a3573b623d6b686abfa1ba
Brennen Nalley <46981701+Rebnnenx@users.noreply.github.com>
2019-04-10 09:49:22 -0400

    fixed something
    I dont know what

--- polynomialOperations/src/Polynomials.java
+++ polynomialOperations/src/Polynomials.java
@@ -2,30 +2,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Polynomials <T extends Comparable<T>>{
+
+class PolyNum implements Comparable<PolyNum>{
+	private int coef = 0, exponent= 0;
 	
-	private class Poly <T extends Comparable<T>>{
-		private int coef, exponent;
+	public PolyNum(int coef, int exp) {
+		this.coef = coef;
+		this.exponent=exp;
 		
-		public Poly(int coef, int exp) {
-			this.coef = coef;
-			this.exponent=exp;
-			
-			return;
-		}
-		public int getCoeficent() {
-			
-		}
+		return;
 	}
+	
+	public int getCoeficent() {
+		return coef;
+	}
+	
+	public int getExponent() {
+		return exponent;
+	}
+
+	@Override
+	public int compareTo(PolyNum o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
+public class Polynomials <T extends Comparable<T>>{
 
 	public static void main(String[] args) throws FileNotFoundException{
 		
 		
 		File list = new File("polynomials.txt");
 	    Scanner fileReader = new Scanner(list);
-	    LinkedList<Poly> poly1 = new LinkedList<Poly>();
-	    LinkedList<Integer> poly2 = new LinkedList<Integer>();
-	    LinkedList<Integer> polyR = new LinkedList<Integer>();
+	    LinkedList<PolyNum> poly1 = new LinkedList<PolyNum>();
+	    LinkedList<PolyNum> poly2 = new LinkedList<PolyNum>();
+	    LinkedList<PolyNum> polyR = new LinkedList<PolyNum>();
 	    
 	    int x, coef, exp;
 	    String input;
@@ -35,15 +46,16 @@ public class Polynomials <T extends Comparable<T>>{
 	    input = fileReader.nextLine();
 	    String terms[] =input.split(" ");
 	    
+	    for (int i=0; i<terms.length;i=+2) {
+	    	PolyNum temp = new PolyNum(Integer.parseInt(terms[i]),Integer.parseInt(terms[i+1]));
+	    	poly1.insertAtBack(temp);
+	    }
+	    
 	    
 	    input = fileReader.nextLine();
 	    terms=input.split(" ");
-	    
-
-	    
-	    
-	    
-	    
 	}
+
+	
 	
 }
